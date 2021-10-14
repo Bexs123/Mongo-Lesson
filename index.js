f6976391ac7c9700aaddb73d1ba3e392ec80b4a9
Rebbecca Fox <foxy2530@gmail.com>
2021-10-14 10:49:27 +0100

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.arg)).argv;
+
+
+const mongoose = require("mongoose");
+mongoose.connect (
+    'mongodb://localhost:27017/my-friends',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
